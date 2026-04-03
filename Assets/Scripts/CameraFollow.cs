4a83160980f90be19cdae0def9acddbd538c1c34
Gunti Hemanth Kumar <gunti.hemanth.kumar.dev@gmail.com>
2026-04-04 00:03:45 +0530

    Create CameraFollow.cs

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+}
