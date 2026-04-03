50aaafd1194cc489f4559378f1f662649208d6b9
Gunti Hemanth Kumar <gunti.hemanth.kumar.dev@gmail.com>
2026-04-04 00:03:53 +0530

    Update missileController.cs

--- Assets/Scripts/missileController.cs
+++ Assets/Scripts/missileController.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class mIssileController : MonoBehaviour
 {
     [SerializeField] private float speed; // Speed of the missile
-    
+    Rigidbody rb; // Reference to the Rigidbody component
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the missile
     }
 
     // Update is called once per frame
@@ -15,6 +15,8 @@ public class mIssileController : MonoBehaviour
     {
         
         transform.Translate(transform.forward * speed * Time.deltaTime); 
+        //rb.AddForce(transform.forward * speed * Time.deltaTime); // Move the missile forward based on its speed
+
 
     }
 }
